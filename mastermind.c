45ce06e9e1afae26a2e90b92afdbf4940e86a360
TJ MacDougall <timothy.macdougall@uoit.net>
2017-03-06 18:14:32 -0500

    Generates random sequence

--- mastermind.c
+++ mastermind.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <regex.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define RED   "\x1B[37;41;1m"
 #define GRN   "\x1B[32;40;1;7m"
@@ -11,64 +12,81 @@
 #define WHT   "\x1B[37;40;1;7m"
 #define BLA   "\x1B[37;40;1m"
 #define RESET "\x1B[0m"
+#define LENGTH 5
+
 
 int main()
 {
 
-	char str[5];
-
-	printf("Enter a string: ");
-	scanf("%s", str);
+	char str[LENGTH];
+	char colours[8] = "RGYBMCWK";
+	
 
 	regex_t regex;
 	int reti;
 
 
-	reti = regcomp(&regex, "[RGYBMCWK]{5}", REG_EXTENDED|REG_NOSUB);
+	reti = regcomp(&regex, "[RrGgYyBbMmCcWwKk]{5}", 
+					REG_EXTENDED | REG_NOSUB);
 	
 	if(reti) {
 		fprintf(stderr, "Could not compile regex\n");
 		exit(1);
 	}
 
-	reti = regexec(&regex, str, 0, NULL, 0);
-	printf("%d\n", reti);
-	if(!reti) {
-		puts("GOOD");
-	} else {
-		puts("BAD");
+
+	// Generate sequence
+	char seq[LENGTH];
+	for(int i=0; i<LENGTH; i++) {
+		int random = rand() % 8;
+		seq[i] = colours[random];
 	}
 
-	for(int i=0; i<5; i++){
-		switch(str[i])
-		{
-			case 'R':
-				printf(RED " R " RESET);
-				break;
-			case 'G':
-				printf(GRN " G " RESET);
-				break;
-			case 'B':
-				printf(BLU " B " RESET);
-				break;
-			case 'M':
-				printf(MAG " M " RESET);
-				break;
-			case 'Y':
-				printf(YEL " Y " RESET);
-				break;
-			case 'C':
-				printf(CYN " C " RESET);
-				break;
-			case 'W':
-				printf(WHT " W " RESET);
-				break;
-			case 'K':
-				printf(BLA " K " RESET);
-				break;
+	puts(seq);
+
+	while(1){
+		printf("Enter your guess: ");
+		scanf("%s", str);
+
+		reti = regexec(&regex, str, 0, NULL, 0);
+		if(!reti) {
+			for(int i=0; i<LENGTH; i++) {
+				str[i] = toupper(str[i]);
+			}
+			for(int i=0; i<LENGTH; i++){
+				switch(str[i])
+				{
+				case 'R':
+					printf(RED " R " RESET);
+					break;
+				case 'G':
+					printf(GRN " G " RESET);
+					break;
+				case 'B':
+					printf(BLU " B " RESET);
+					break;
+				case 'M':
+					printf(MAG " M " RESET);
+					break;
+				case 'Y':
+					printf(YEL " Y " RESET);
+					break;
+				case 'C':
+					printf(CYN " C " RESET);
+					break;
+				case 'W':
+					printf(WHT " W " RESET);
+					break;
+				case 'K':
+					printf(BLA " K " RESET);
+					break;
+				}
+			}
+			printf("\n");
+		} else {
+			puts("Invalid input");
 		}
 	}
-	printf("\n");
 
   	return 0;
 }
